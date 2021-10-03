cee2e18b90fb28d1c25b56ae50a1b2073397632d
desy_sito <alexferrara74@gmail.com>
2021-10-03 20:55:29 +0200

    prova

--- public/js/script.js
+++ public/js/script.js
@@ -3,6 +3,7 @@ function openNav() {
   document.getElementById("mySidenav").style.width = "300px";
 }
 
+
 /* Set the width of the side navigation to 0 */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
